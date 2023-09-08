a8630d03cfa7e94c64db1c418b1c0faff15afd5e
Henrique Santos <henrique.santos@indt.org.br>
2023-09-08 16:38:19 -0400

    initial

--- src/modules/user/dtos/create.ts
+++ src/modules/user/dtos/create.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const CreateUserSchema = z.object({
   name: z.string().min(3).max(255),
-  registration: z.string().min(3).max(255),
-  control: z.string().min(3).max(255),
+  registration: z.string().min(3).max(255).regex(/^\d+$/),
+  control: z.string().min(3).max(255).regex(/^\d+$/),
   password: z.string().min(3).max(255),
   profile: z.string().min(3).max(255),
   role: z.enum(["adm", "operator", "tecnico"]),
